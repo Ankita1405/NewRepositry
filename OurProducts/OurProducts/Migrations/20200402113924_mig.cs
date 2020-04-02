a43c585ce22d65195cc3e205fbd40fcbedc5d368
Ankita1405 <ankusharrma007.as@gmail.com>
2020-04-02 17:12:35 +0530

    After adding mig and db update

---
+++ OurProducts/OurProducts/Migrations/20200402113924_mig.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OurProducts.Migrations
+{
+    public partial class mig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Pid = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Pname = table.Column<string>(nullable: true),
+                    Pprice = table.Column<float>(nullable: false),
+                    PDesc = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Pid);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}

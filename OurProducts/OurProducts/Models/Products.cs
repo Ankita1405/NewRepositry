838c8f42d9e345621a45a0f4a4fcbb0e24701ed8
Ankita1405 <ankusharrma007.as@gmail.com>
2020-04-02 16:10:57 +0530

    After adding model

---
+++ OurProducts/OurProducts/Models/Products.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace OurProducts.Models
+{
+    [Table("Products")]
+    public class Products
+    {
+        [Key]
+        public int Pid { get; set; }
+        public string Pname { get; set; }
+        public float Pprice { get; set; }
+        public string PDesc { get; set; }
+
+
+    }
+}
